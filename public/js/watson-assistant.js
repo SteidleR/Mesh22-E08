9cc6858badc00a19d45b9667b939ada054be990e
vap0r <inf19152@lehre.dhbw-stuttgart.de>
2022-03-12 16:33:01 +0100

    watson chat wip

--- public/js/watson-assistant.js
+++ public/js/watson-assistant.js
@@ -1,5 +1,5 @@
 window.watsonAssistantChatOptions = {
-    integrationID: "98ac027c-8aa2-45ee-a3a8-8b4ce6ae51f4", // The ID of this integration.
+    integrationID: "a2d99168-2b06-4a23-8c8b-2ca8d46b0ef2", // The ID of this integration.
     region: "eu-gb", // The region your integration is hosted in.
     serviceInstanceID: "6d0a6a10-f913-46f5-852e-95475e721848", // The ID of your service instance.
     onLoad: function(instance) { instance.render(); }
